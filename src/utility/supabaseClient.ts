ce25f735dfa97aca3f1a1cc22f52d330ad2eafb1
Bryan Gomes <bryanpgomes@gmail.com>
2023-09-05 16:23:12 -0400

    Add env variables for future uses

--- src/utility/supabaseClient.ts
+++ src/utility/supabaseClient.ts
@@ -1,9 +1,8 @@
 import { createClient } from "@refinedev/supabase";
+console.log(import.meta.env)
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
-
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;;
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
   db: {
     schema: "public",
